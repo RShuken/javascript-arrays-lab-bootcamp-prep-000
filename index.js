e46d1e7bcf881e3cf55765b88a788ddec7417cdd
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 03:22:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ function destructivelyRemoveFirstKitten(name) {
 }
 // appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
 function appendKitten(name) {
-  kittens.push(name);
-  return kittens;
+  return 
 }
 //prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
 function prependKiteen(name) {
