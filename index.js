2077e6ba9e592057a6f0d9972967d288b99eeba0
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 02:51:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 // Add your functions and code here
 //appends a kitten to the end of the kittens array:
 function destructivelyAppendKitten(name) {
-  kittens = kittens.pop(name);
+  kittens = kittens.push(name);
   return kittens;
 }
 // prepends a kitten to the beginningof the kittens array:
