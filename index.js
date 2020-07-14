033bb0671da947ff7c2fe5640ce9cced4a3a45ac
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 03:13:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyPrependKitten(name) {
 }
 // removes the last kitten from the kittens array:
 function destructivelyRemoveLastKitten(name) {
-  kittens = kittens.slice[name, kittens.lenght - 1 ];
+  kittens = kittens.slice(name, kittens.lenght - 1);
   return kittens;
 }
 //removes the First kitten from the kittens array:
