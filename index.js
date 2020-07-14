ae7e505264fa68486aa6c6ec2bb29b8535f3d944
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 02:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
-//
+//appends a kitten to the end of the kittens array:
 function destructivelyAppendKitten(name) {
   
 }
