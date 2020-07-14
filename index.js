ca02dbdfd60c9c7d32fd24a29a6af62d46f98e7c
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 02:54:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyAppendKitten(name) {
   kittens = kittens.splice(name);
   return kittens;
 }
-// prepends a kitten to the beginningof the kittens array:
+// prepends a kitten to the beginning of the kittens array:
 function destructivelyPrependKitten(name) {
   
 }
