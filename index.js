a10be795b9d007f41bb80a6f1bee73e0dcce2887
gorgeous-monitor-5108 <ryanshuken@gmail.com>
2020-07-14 02:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function appendKitten(name) {
 function prependKiteen(name) {
   
 }
-
+//
 function removeLastKitten() {
   
 }
-// 
+// removes the first kitten from the kittens arrayand returns a new array, leaving the kittens array unchanged:
 function removeFirstKitten() {
   
 }
